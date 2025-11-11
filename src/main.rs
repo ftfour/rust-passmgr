51f261132cc1844eb62e3f751cb6aa2699dba3ad
Ersan Egorov <koilfrost@gmail.com>
2025-11-11 11:20:07 +0900

    Добавил команду инициализации

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,63 @@ mod crypto;
 
 use model::{Vault, Entry, FileFormat};
 use storage::{load_fileformat, save_fileformat};
+use crypto::{derive_key, generate_salt, decrypt_vault, encrypt_vault};
+
+use clap::{Parser, Subcommand};
+use base64::{engine::general_purpose, Engine as _};
+use rpassword;
 use std::path::PathBuf;
 use anyhow::Result;
-use crypto::{derive_key, generate_salt, decrypt_vault, encrypt_vault};
-use std::str;
+
+#[derive(Parser)]
+#[command(name="rust-passmgr")]
+#[command(about="Мини-менеджер паролей на Rust")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init {
+        #[arg(short, long, default_value = "vault.json")]
+        file: PathBuf,
+    },
+}
 
 fn main() -> Result<()> {
-    let mut v = Vault::default();
-    v.entries.insert(
-        "example.com".into(),
-        Entry {
-            login: "user".into(),
-            password: "pass123".into(),
-            notes: Some("demo".into()),
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Init { file } => {
+            if file.exists() {
+                println!("Файл {:?} существует, не перезаписываю.", file);
+                return Ok(());
+            }
+            
+            let pass1 = rpassword::prompt_password("Введите мастер-пароль: ")?;
+            let pass2 = rpassword::prompt_password("Повторите пароль: ")?;
+            if pass1 != pass2 {
+                println!("Пароли не совпадают");
+                return Ok(());
+            }
+
+            let salt = generate_salt();
+
+            let vault = Vault::default();
+
+            let blob = encrypt_vault(&vault, &pass1, &salt)?;
+
+            let ff = FileFormat {
+                version: 1,
+                salt: general_purpose::STANDARD.encode(&salt),
+                blob: general_purpose::STANDARD.encode(&blob),
+            };
+
+            save_fileformat(&file, &ff)?;
+            println!("Хранилище создано {:?}", file);
         }
-    );
-
-    let password = "test123";
-    let salt = generate_salt();
-
-    let blob = encrypt_vault(&v, password, &salt)?;
-    println!("Encrypted blob size: {}", blob.len());
-
-    let v2 = decrypt_vault(&blob, password, &salt)?;
-    println!("Roundtrip ok, entries: {}", v2.entries.len());
+    }
 
     Ok(())
 }
